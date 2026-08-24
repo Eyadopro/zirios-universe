110f2052f8777f5ca07bdf1ae87b44811f7e9b0f
Eyad <167235158+Eyadopro@users.noreply.github.com>
2026-08-24 22:09:18 +0300

    Part 6: Integrate CameraDirector

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -1,22 +1,16 @@
 import { WorldManager } from './WorldManager'
 import { TypographyEngine } from './TypographyEngine/TypographyEngine'
+import { CameraDirector } from './CameraDirector'
 
 /**
  * Experience — root of the 3D scene.
- * Hosts WorldManager + TypographyEngine + future systems.
  */
 export function Experience() {
   return (
     <>
       <WorldManager />
       <TypographyEngine />
-      {/* Future systems:
-          <ParticleEngine />
-          <PhysicsEngine />
-          <CameraDirector />
-          <QuoteEngine />
-          <PostFXManager />
-      */}
+      <CameraDirector />
     </>
   )
 }
